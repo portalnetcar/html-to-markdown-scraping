13499f0ffaea199385afbb0d88e4dbe88f97152a
leosavio <leosavio@leosavio.com>
2023-05-30 22:45:01 -0300

    refactoring with class, encapsulating methods and update imgs

--- markdowner.py
+++ markdowner.py
@@ -11,41 +11,53 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # To ignore SSL certificate errors
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = 'https://cloudofthings.net/lab-10/'
+class MarkdownGenerator:
+    def __init__(self):
+        self.md_content = ''
+        self.image_counter = 0  # To keep track of image numbers
+        self.ssl_context = ssl._create_unverified_context()
 
-# Fetch the content from url, ignoring check of SSL certificate
-response = requests.get(url, verify=False)
-soup = BeautifulSoup(response.text, 'html.parser')
+    def handle_node(self, node):
+        if node.name == 'img':
+            # Handle image node
+            img_url = node['src']
+            
+            # Download image, ignoring SSL certificate validation
+            with urllib.request.urlopen(img_url, context=self.ssl_context) as response, open(f'images/image_{self.image_counter}.jpg', 'wb') as out_file:
+                data = response.read()  # a `bytes` object
+                out_file.write(data)
+            
+            # Append image to markdown
+            self.md_content += f'![Image {self.image_counter}](images/image_{self.image_counter}.jpg)\n\n'
+            self.image_counter += 1
+        elif node.name is None:
+            # Append text node to markdown
+            self.md_content += node + '\n\n'
+        else:
+            # Recursively handle children of other nodes
+            for child in node.children:
+                self.handle_node(child)
 
-# Find the article section by css selector
-article = soup.select('article')
+    def generate_markdown(self, url, css_selector):
+        # Fetch the content from url, ignoring check of SSL certificate
+        response = requests.get(url, verify=False)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-# Create a directory for images
-if not os.path.exists('images'):
-    os.makedirs('images')
+        # Find the article section by css selector
+        article = soup.select(css_selector)
 
-md_content = ''
-for section in article:
-    # Append text content to markdown
-    md_content += section.get_text() + '\n\n'
-    
-    # Find all image tags
-    images = section.find_all('img')
+        # Create a directory for images
+        if not os.path.exists('images'):
+            os.makedirs('images')
 
-    # Create an unverified SSL context for image downloading
-    ssl_context = ssl._create_unverified_context()
+        for section in article:
+            self.handle_node(section)
 
-    for i, img in enumerate(images):
-        img_url = img['src']
-        
-        # Download image, ignoring SSL certificate validation
-        with urllib.request.urlopen(img_url, context=ssl_context) as response, open(f'images/image_{i}.jpg', 'wb') as out_file:
-            data = response.read()  # a `bytes` object
-            out_file.write(data)
-        
-        # Append image to markdown
-        md_content += f'![Image {i}](images/image_{i}.jpg)\n\n'
+        # Write to markdown file
+        with open('article.md', 'w') as f:
+            f.write(self.md_content)
 
-# Write to markdown file
-with open('article.md', 'w') as f:
-    f.write(md_content)
+url = 'https://...'
+css_selector = 'article'  # replace this with your actual CSS selector
+generator = MarkdownGenerator()
+generator.generate_markdown(url, css_selector)
